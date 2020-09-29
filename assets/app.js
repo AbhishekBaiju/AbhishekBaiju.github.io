73362c9de2372ef8778da75cd81d4fbb55ca3d52
Abhishek Baiju <johnponnamkudam@gmail.com>
2020-09-29 22:24:34 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,27 @@
 function SubForm (){
+    document.querySelector('#reset').innerHTML = 'Submitting...';
     $.ajax({
         url:'https://api.apispreadsheets.com/data/1622/',
         type:'post',
         data:$("#myForm").serializeArray(),
         success: function success(){
+          
           document.getElementById('msg').innerHTML = "Submitted Successfully";
+          document.getElementById('msg').style.color='Green';
+          document.querySelector('#reset').innerHTML = 'Submitted';
           document.getElementById("reset").disabled = true;
+          document.getElementById('reset').style.backgroundColor='Green';
+          document.getElementById("reset").value="Done";
           document.getElementById("myForm").reset();
           
-          setTimeout("location.href = 'https://tinkerhub.org/';",1500);
+          
+          
+          
         },
         error: function error(){
           document.getElementById('msg').innerHTML = "hmmm sm sm wrong";
+          location.reload();
+          setTimeout("location.href = 'https://tinkerhub.org/';",1500);
         }
     });
 } 
